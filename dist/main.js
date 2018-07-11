35e36980152ae0202fdf54c0bbfba32f02fd61a7
N.E.C <beholdnec@gmail.com>
2018-07-11 12:26:56 -0700

    Start writing code to draw objects

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,40 @@
 console.log('Hello, world!');
 var canvas = document.getElementById('main-canvas');
 var gl = canvas.getContext('webgl2');
+function compileShader(shaderType, source) {
+    var shader = gl.createShader(shaderType);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        console.error(gl.getShaderInfoLog(shader));
+        return null;
+    }
+    return shader;
+}
+function compileProgram(vertexShaderSource, fragmentShaderSource) {
+    var vertexShader = compileShader(gl.VERTEX_SHADER, vertexShaderSource);
+    var fragmentShader = compileShader(gl.FRAGMENT_SHADER, fragmentShaderSource);
+    var shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+        console.error(gl.getProgramInfoLog(shaderProgram));
+        throw ("Failed to initialize shaders");
+    }
+    return shaderProgram;
+}
+var SFX_VERTEX_SHADER_SOURCE = "\nprecision mediump float;\n\nattribute vec3 aPosition;\n\nuniform mat4 uModelMatrix;\nuniform mat4 uViewProjMatrix;\n\nvarying vec3 vWorldPosition;\n\nvoid main()\n{\n    vWorldPosition = (uModelMatrix * vec4(aPosition, 1.)).xyz;\n    gl_Position = uViewProjMatrix * vec4(vWorldPosition, 1.);\n}\n";
+var SFX_FRAGMENT_SHADER_SOURCE = "\nprecision mediump float;\n\nvarying vec3 vWorldPosition;\n\nvoid main()\n{\n    gl_FragColor = vec4(0., 1., 0., 1.);\n}\n";
+var SFXShader = /** @class */ (function () {
+    function SFXShader() {
+        this.program = compileProgram(SFX_VERTEX_SHADER_SOURCE, SFX_FRAGMENT_SHADER_SOURCE);
+        this.aPosition = gl.getAttribLocation(this.program, 'aPosition');
+        this.uModelMatrix = gl.getUniformLocation(this.program, 'uModelMatrix');
+        this.uViewProjMatrix = gl.getUniformLocation(this.program, 'uViewProjMatrix');
+    }
+    return SFXShader;
+}());
 var VertexCmd;
 (function (VertexCmd) {
     VertexCmd[VertexCmd["Plain"] = 4] = "Plain";
@@ -9,6 +43,8 @@ var VertexCmd;
 })(VertexCmd || (VertexCmd = {}));
 var SFXObject = /** @class */ (function () {
     function SFXObject(rom, verticesOffset, facesOffset) {
+        this.numVertices = 0;
+        this.shader = new SFXShader();
         var dv = new DataView(rom);
         // Load vertices
         var vertices = [];
@@ -57,7 +93,18 @@ var SFXObject = /** @class */ (function () {
                     throw new Error("Unknown vertex entry type 0x" + type.toString(16));
             }
         }
+        this.vertexBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+        this.numVertices = vertices.length / 3;
     }
+    SFXObject.prototype.render = function () {
+        gl.useProgram(this.shader.program);
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
+        gl.enableVertexAttribArray(this.shader.aPosition);
+        gl.vertexAttribPointer(this.shader.aPosition, 3, gl.FLOAT, false, 0, 0);
+        gl.drawArrays(gl.POINTS, 0, this.numVertices);
+    };
     return SFXObject;
 }());
 function loadRom(rom) {
@@ -68,6 +115,7 @@ function loadRom(rom) {
     // Note: Many sources assume there is a 0x200-byte padding at the beginning of the ROM.
     // TODO: Detect and handle this padding.
     var sfxObj = new SFXObject(rom, 0x66001, 0x66042);
+    sfxObj.render();
 }
 var fileInput = document.getElementById('file-input');
 fileInput.onchange = function (event) {
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,88 @@ console.log('Hello, world!')
 const canvas = <HTMLCanvasElement>document.getElementById('main-canvas')
 const gl = <WebGL2RenderingContext>canvas.getContext('webgl2')
 
+function compileShader(shaderType: number, source: string): WebGLShader
+{
+    const shader = gl.createShader(shaderType)
+    gl.shaderSource(shader, source)
+    gl.compileShader(shader)
+
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS))
+    {
+        console.error(gl.getShaderInfoLog(shader))
+        return null
+    }
+
+    return shader
+}
+
+function compileProgram(vertexShaderSource: string, fragmentShaderSource: string): WebGLProgram
+{
+    const vertexShader = compileShader(gl.VERTEX_SHADER, vertexShaderSource)
+    const fragmentShader = compileShader(gl.FRAGMENT_SHADER, fragmentShaderSource)
+
+    const shaderProgram = gl.createProgram()
+    gl.attachShader(shaderProgram, vertexShader)
+    gl.attachShader(shaderProgram, fragmentShader)
+    gl.linkProgram(shaderProgram)
+
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS))
+    {
+        console.error(gl.getProgramInfoLog(shaderProgram))
+        throw("Failed to initialize shaders")
+    }
+
+    return shaderProgram
+}
+
+const SFX_VERTEX_SHADER_SOURCE =
+`
+precision mediump float;
+
+attribute vec3 aPosition;
+
+uniform mat4 uModelMatrix;
+uniform mat4 uViewProjMatrix;
+
+varying vec3 vWorldPosition;
+
+void main()
+{
+    vWorldPosition = (uModelMatrix * vec4(aPosition, 1.)).xyz;
+    gl_Position = uViewProjMatrix * vec4(vWorldPosition, 1.);
+}
+`
+
+const SFX_FRAGMENT_SHADER_SOURCE =
+`
+precision mediump float;
+
+varying vec3 vWorldPosition;
+
+void main()
+{
+    gl_FragColor = vec4(0., 1., 0., 1.);
+}
+`
+
+class SFXShader
+{
+    aPosition: number
+    uModelMatrix: WebGLUniformLocation
+    uViewProjMatrix: WebGLUniformLocation
+
+    program: WebGLProgram
+
+    constructor()
+    {
+        this.program = compileProgram(SFX_VERTEX_SHADER_SOURCE, SFX_FRAGMENT_SHADER_SOURCE)
+
+        this.aPosition = gl.getAttribLocation(this.program, 'aPosition')
+        this.uModelMatrix = gl.getUniformLocation(this.program, 'uModelMatrix')
+        this.uViewProjMatrix = gl.getUniformLocation(this.program, 'uViewProjMatrix')
+    }
+}
+
 enum VertexCmd
 {
     Plain = 0x04,
@@ -12,8 +94,14 @@ enum VertexCmd
 
 class SFXObject
 {
+    vertexBuffer: WebGLBuffer
+    numVertices: number = 0
+    shader: SFXShader
+
     constructor(rom: ArrayBuffer, verticesOffset: number, facesOffset: number)
     {
+        this.shader = new SFXShader()
+
         const dv = new DataView(rom)
 
         // Load vertices
@@ -70,6 +158,22 @@ class SFXObject
                 throw new Error(`Unknown vertex entry type 0x${type.toString(16)}`)
             }
         }
+
+        this.vertexBuffer = gl.createBuffer()
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer)
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW)
+        this.numVertices = vertices.length / 3
+    }
+
+    render()
+    {
+        gl.useProgram(this.shader.program)
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer)
+        gl.enableVertexAttribArray(this.shader.aPosition)
+        gl.vertexAttribPointer(this.shader.aPosition, 3, gl.FLOAT, false, 0, 0)
+
+        gl.drawArrays(gl.POINTS, 0, this.numVertices)
     }
 }
 
@@ -84,6 +188,8 @@ function loadRom(rom: ArrayBuffer)
     // Note: Many sources assume there is a 0x200-byte padding at the beginning of the ROM.
     // TODO: Detect and handle this padding.
     const sfxObj = new SFXObject(rom, 0x66001, 0x66042)
+
+    sfxObj.render()
 }
 
 const fileInput = <HTMLInputElement>document.getElementById('file-input')

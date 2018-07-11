5675f410a018db7bf21af6e1465a98293b5ccca9
N.E.C <beholdnec@gmail.com>
2018-07-11 11:43:32 -0700

    Load vertices

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,73 @@
 console.log('Hello, world!');
+var canvas = document.getElementById('main-canvas');
+var gl = canvas.getContext('webgl2');
+var VertexCmd;
+(function (VertexCmd) {
+    VertexCmd[VertexCmd["Plain"] = 4] = "Plain";
+    VertexCmd[VertexCmd["End"] = 12] = "End";
+    VertexCmd[VertexCmd["XFlipped"] = 56] = "XFlipped";
+})(VertexCmd || (VertexCmd = {}));
+var SFXObject = /** @class */ (function () {
+    function SFXObject(rom, verticesOffset, facesOffset) {
+        var dv = new DataView(rom);
+        // Load vertices
+        var vertices = [];
+        var cursor = verticesOffset;
+        var done = false;
+        while (!done) {
+            var type = dv.getUint8(cursor);
+            cursor++;
+            switch (type) {
+                case VertexCmd.Plain:
+                    {
+                        var num = dv.getUint8(cursor);
+                        cursor++;
+                        for (var i = 0; i < num; i++) {
+                            var x = dv.getInt8(cursor);
+                            cursor++;
+                            var y = dv.getInt8(cursor);
+                            cursor++;
+                            var z = dv.getInt8(cursor);
+                            cursor++;
+                            vertices.push(x, y, z);
+                        }
+                        break;
+                    }
+                case VertexCmd.End:
+                    console.log("End of verts");
+                    done = true;
+                    break;
+                case VertexCmd.XFlipped:
+                    {
+                        var num = dv.getUint8(cursor);
+                        cursor++;
+                        for (var i = 0; i < num; i++) {
+                            var x = dv.getInt8(cursor);
+                            cursor++;
+                            var y = dv.getInt8(cursor);
+                            cursor++;
+                            var z = dv.getInt8(cursor);
+                            cursor++;
+                            vertices.push(x, y, z);
+                            vertices.push(-x, y, z);
+                        }
+                        break;
+                    }
+                default:
+                    throw new Error("Unknown vertex entry type 0x" + type.toString(16));
+            }
+        }
+    }
+    return SFXObject;
+}());
 function loadRom(rom) {
     console.log("Loading ROM...");
+    gl.clearColor(0.1, 0.2, 0.3, 1);
+    gl.clearDepth(1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    // Note: Many sources assume there is a 0x200-byte padding at the beginning of the ROM.
+    // TODO: Detect and handle this padding.
+    var sfxObj = new SFXObject(rom, 0x66001, 0x66042);
 }
 var fileInput = document.getElementById('file-input');
 fileInput.onchange = function (event) {
@@ -9,7 +76,7 @@ fileInput.onchange = function (event) {
     var reader = new FileReader();
     reader.onloadend = function (evt) {
         if (evt.target.readyState == FileReader.DONE) {
-            loadRom(new Uint8Array(reader.result));
+            loadRom(reader.result);
         }
     };
     reader.readAsArrayBuffer(file);
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,89 @@
 console.log('Hello, world!')
 
-function loadRom(rom: Uint8Array)
+const canvas = <HTMLCanvasElement>document.getElementById('main-canvas')
+const gl = <WebGL2RenderingContext>canvas.getContext('webgl2')
+
+enum VertexCmd
+{
+    Plain = 0x04,
+    End = 0x0C,
+    XFlipped = 0x38,
+}
+
+class SFXObject
+{
+    constructor(rom: ArrayBuffer, verticesOffset: number, facesOffset: number)
+    {
+        const dv = new DataView(rom)
+
+        // Load vertices
+        const vertices = []
+        var cursor = verticesOffset
+        var done = false
+        while (!done)
+        {
+            const type = dv.getUint8(cursor)
+            cursor++
+
+            switch (type)
+            {
+            case VertexCmd.Plain:
+            {
+                const num = dv.getUint8(cursor)
+                cursor++
+                for (let i = 0; i < num; i++)
+                {
+                    const x = dv.getInt8(cursor)
+                    cursor++
+                    const y = dv.getInt8(cursor)
+                    cursor++
+                    const z = dv.getInt8(cursor)
+                    cursor++
+
+                    vertices.push(x, y, z)
+                }
+                break
+            }
+            case VertexCmd.End:
+                console.log(`End of verts`)
+                done = true
+                break
+            case VertexCmd.XFlipped:
+            {
+                const num = dv.getUint8(cursor)
+                cursor++
+                for (let i = 0; i < num; i++)
+                {
+                    const x = dv.getInt8(cursor)
+                    cursor++
+                    const y = dv.getInt8(cursor)
+                    cursor++
+                    const z = dv.getInt8(cursor)
+                    cursor++
+
+                    vertices.push(x, y, z)
+                    vertices.push(-x, y, z)
+                }
+                break
+            }
+            default:
+                throw new Error(`Unknown vertex entry type 0x${type.toString(16)}`)
+            }
+        }
+    }
+}
+
+function loadRom(rom: ArrayBuffer)
 {
     console.log(`Loading ROM...`)
+
+    gl.clearColor(0.1, 0.2, 0.3, 1)
+    gl.clearDepth(1.0)
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+    // Note: Many sources assume there is a 0x200-byte padding at the beginning of the ROM.
+    // TODO: Detect and handle this padding.
+    const sfxObj = new SFXObject(rom, 0x66001, 0x66042)
 }
 
 const fileInput = <HTMLInputElement>document.getElementById('file-input')
@@ -16,7 +97,7 @@ fileInput.onchange = function (event)
     {
         if (evt.target.readyState == FileReader.DONE)
         {
-            loadRom(new Uint8Array(reader.result))
+            loadRom(reader.result)
         }
     }
 
